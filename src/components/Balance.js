9f588f2ee35a248c6d62863761975a3414cd9971
Yashs Tiwari <yashstiwari03@gmail.com>
2023-11-09 17:58:02 +0530

    added balance functionality

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useGlobalContext } from "./context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useGlobalContext();
+
+  const amount = transactions.map((transaction) => transaction.amount);
+  const balance = amount
+    .reduce((acc, transaction) => (acc += transaction), 0)
+    .toFixed(2);
+
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${balance}</h1>
     </div>
   );
 };
