7da032bca982da07bd6368b7ca44473cf9d881f3
Yashs Tiwari <yashstiwari03@gmail.com>
2023-11-09 18:26:13 +0530

    added delete Transaction action

---
+++ src/components/Transaction.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useGlobalContext } from "./context/GlobalState";
+
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useGlobalContext();
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
